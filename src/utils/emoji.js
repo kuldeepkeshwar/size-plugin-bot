97ff4995d5c7f23f7727e9bb7db259d27d87a9f8
kuldeepkeshwar <kuldeepkeshwar@gmail.com>
2019-08-26 01:39:41 +0530

    fixes #25

---
+++ src/utils/emoji.js
@@ -0,0 +1,13 @@
+const list = require('emojis-list');
+
+const total = list.length;
+
+function random(n = 2) {
+  const items = [];
+  for (let index = 0; index < n; index += 1) {
+    const i = Math.floor(Math.random() * total);
+    items.push(list[i]);
+  }
+  return items;
+}
+module.exports = { random };
